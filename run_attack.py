92ffb0a4b280f10263716c194bf7ecaa736acaef
Ella Luedeke <68171464+ellaluedeke@users.noreply.github.com>
2025-06-25 14:15:51 -0400

    Added LoTL functionality

--- run_attack.py
+++ run_attack.py
@@ -1,14 +1,16 @@
 import argparse
 from attacks.api_spoof import run_spoof_attack
 from attacks.power_disable import run_power_disable_attack
-
+from attacks.lotl_install import run_lotl_attack
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Run simulated Enphase attacks")
-    parser.add_argument("--mode", choices=["spoof", "disable", "lotl"], required=True, help="Attack type")
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser(description="Run simulated Enphase attacks")
+	parser.add_argument("--mode", choices=["spoof", "disable", "lotl"], required=True, help="Attack type")
+	args = parser.parse_args()
 
-    if args.mode == "spoof":
-        run_spoof_attack()
-    elif args.mode == "disable":
-        run_power_disable_attack()
+	if args.mode == "spoof":
+		run_spoof_attack()
+	elif args.mode == "disable":
+		run_power_disable_attack()
+	elif args.mode == "lotl":
+		run_lotl_attack()
 
